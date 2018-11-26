28f704457e1d7abb85dccfeca8d750c90828d7bb
r.ishibashi <ryuhei.ishibashi@bizreach.co.jp>
2018-11-26 18:29:55 +0900

    init

---
+++ aquahub/client/src/main/scala/com/rysh/aquahub/ScalaJSExample.scala
@@ -0,0 +1,11 @@
+package com.rysh.aquahub
+
+import com.rysh.aquahub.shared.SharedMessages
+import org.scalajs.dom
+
+object ScalaJSExample {
+
+  def main(args: Array[String]): Unit = {
+    dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
+  }
+}
---
+++ aquahub/shared/src/main/scala/com/rysh/aquahub/shared/SharedMessages.scala
@@ -0,0 +1,5 @@
+package com.rysh.aquahub.shared
+
+object SharedMessages {
+  def itWorks = "It works!"
+}
