3743870186bc7dea9127ac9fc870f61eadbf69d4
r.ishibashi <ryuhei.ishibashi@bizreach.co.jp>
2018-11-30 13:17:42 +0900

    add article page

--- aquahub/server/app/controllers/Application.scala
+++ aquahub/server/app/controllers/Application.scala
@@ -4,15 +4,27 @@ import com.rysh.aquahub.shared.SharedMessages
 import javax.inject._
 import models.Article
 import play.api.mvc._
-import scalikejdbc.AutoSession
+import scalikejdbc._
 
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
     implicit val session: AutoSession.type = AutoSession
-    println(Article.findAll())
-    Ok(views.html.index(SharedMessages.itWorks))
+    val articles: Seq[Article]             = Article.findAll()
+    println(articles)
+    Ok(views.html.index(SharedMessages.itWorks, articles))
   }
 
+  def article(id: String) = Action {
+    implicit val session: AutoSession.type = AutoSession
+
+    val maybeArticle: Option[Article] = Article.findBy(sqls" article_id = ${id} ")
+
+    maybeArticle match {
+      case Some(article) => Ok(views.html.article(article))
+      case None          => NotFound
+    }
+
+  }
 }
