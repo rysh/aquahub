0eb8d0ee48bc35f0daa953e167ead6aa0374e105
rysh <rysh.cact@gmail.com>
2018-12-06 19:16:31 +0900

    fix list image css

--- aquahub/client/src/main/scala/ScalaJSExample.scala
+++ aquahub/client/src/main/scala/ScalaJSExample.scala
@@ -8,8 +8,6 @@ object ScalaJSExample {
     Option(document.getElementById("top_image"))
       .map(_.asInstanceOf[HTMLImageElement])
       .foreach(image => image.onload = _ => clipTopImage(image))
-    val nodeList = document.getElementsByName("list_image")
-    (0 until nodeList.length).map(nodeList.item(_).asInstanceOf[HTMLImageElement]).foreach(image => image.onload = _ => clipListImage(image))
   }
 
   private def clipTopImage(image: HTMLImageElement): Unit = {
@@ -17,13 +15,5 @@ object ScalaJSExample {
     val clientWidth = container.parentElement.clientWidth
     image.style = ClipTopImage.clip(image.naturalHeight, image.naturalWidth, clientWidth)
     container.style = s"height: ${ClipTopImage.gHeight(clientWidth)}px;position: relative;"
-  }
-
-  private def clipListImage(image: HTMLImageElement): Unit = {
-    val clientWidth = document.getElementById("main").clientWidth / 5
-
-    image.width = clientWidth
-    image.height = ClipTopImage.gHeight(clientWidth)
-    image.style = "display: block;"
   }
 }
